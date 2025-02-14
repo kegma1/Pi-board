1ed1ed0a7efedd27dc65ccd312323eecc01d72c5
Kennet Mathisen <kennetam02@gmail.com>
2025-02-14 20:30:31 +0100

    continued some shit, baseing it on https://github.com/fatihak/InkyPi/blob/main/src/inkypi.py

--- __init__.py
+++ __init__.py
@@ -1,18 +1,39 @@
-from PIL import Image, ImageDraw, ImageFont
-from time import sleep
+# set up logging
+import os, logging.config
+logging.config.fileConfig(os.path.join(os.path.dirname(__file__), 'config', 'logging.conf'))
 
+# suppress warning from inky library https://github.com/pimoroni/inky/issues/205
+import warnings
+warnings.filterwarnings("ignore", message=".*Busy Wait: Held high.*")
+
+from PIL import Image, ImageDraw, ImageFont
+import os
+import logging
+import threading
+from time import sleep
+from flask import Flask
 ## 800 x 480 
+logger = logging.getLogger(__name__)
+logger.info("Starting web server")
+
+app = Flask(__name__)
+
+import routes.index
 
 
 def main():
-    img = Image.new("RGBA", (800, 480), (255, 255, 255, 255))
-    d = ImageDraw.Draw(img)
+    while True:
+        img = Image.new("RGBA", (800, 480), (255, 255, 255, 255))
+        d = ImageDraw.Draw(img)
 
-    d.text((10, 10), "Hello", fill=(255, 255, 255, 128))
+        d.text((10, 10), "Hello", fill=(255, 255, 255, 128))
 
-    img.show()
+        img.show()
+        sleep(80)
 
 if __name__ == "__main__":
-    while True:
-        main()
-        sleep(80)
+    from werkzeug.serving import is_running_from_reloader
+
+    app.run()
+
+        
